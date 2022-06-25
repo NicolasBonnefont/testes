47b6bde56e95ecf031eff4ff73f747094c3b111f
BrenoNovais <breno.novais@hotmail.com>
2022-06-25 19:01:23 -0300

    home

---
+++ semana1/proj11/src/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import routes from "./routes/usuarios.js"
+
+const app = express()
+
+app.use(express.json())
+
+app.use(routes)
+
+app.get("/", (req, res) => res.json({"message": "você chegou aqui!"}))
+
+app.listen(3000, console.log("Servidor aberto na porta 3000"))
+
+// po
---
+++ semana1/proj11/src/usuarios.js
@@ -0,0 +1,45 @@
+export const usuarios = [
+    {   
+        id: 1,
+        nome: "Bruno",
+        email: "bvinicius@gmail.com",
+        senha: "senha123"
+    },
+
+    {
+        id: 2,
+        nome: "Nicolas",
+        email: "nicolasserge@gmail.com",
+        senha: "senha123"
+    },
+
+    {
+        id: 3,
+        nome: "Gustavo",
+        email: "gustavingoku@gmail.com",
+        senha: "senha123"
+    },
+
+    {
+        id: 4,
+        nome: "Rodrigo",
+        email: "rodrigodigo@gmail.com",
+        senha: "senha123"
+    },
+
+    {
+        id: 5,
+        nome: "Guilherme",
+        email: "guilhermin7l@gmail.com",
+        senha: "senha123"
+    },
+
+    {
+        id: 6,
+        nome: "Viktor",
+        email: "naoseimais@gmail.com",
+        senha: "senha123"
+    },
+
+]
+export default usuarios
